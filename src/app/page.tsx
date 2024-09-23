b8c6c354cafea2ca56eb46379e637ced02e63403
DANILO GOMES <danilo-c-g@hotmail.com>
2024-09-23 18:46:43 -0300

    add novo tema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import BackToTopButton from "@/components/BackToTopButton";
-import Banner from "@/components/Banner";
-import Header from "@/components/Header";
+"use client";
+import { Navbar } from "@/components/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <Banner />
-      <div style={{ height: "1000px" }} />
-      <BackToTopButton />
-    </>
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
   );
 }
