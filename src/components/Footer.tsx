cc6391c9b4283dbe6ad720182f98cc886aa10932
Danilo Gomes <danilogomes@fieg.com.br>
2024-09-25 09:18:23 -0300

    add alinhamento no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,10 @@ import LogoLight from "../app/assets/logo-light.png";
 export const Footer = () => {
   const { theme } = useThemeStore();
   return (
-    <footer id="footer">
+    <footer id="footer " className="flex flex-col items-center">
       <hr className="w-11/12 mx-auto" />
-      <section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
-        <div className="col-span-full xl:col-span-2">
+      <section className="container py-20 pl-5  grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
+        <div className="col-span-full md:col-span-1 xl:col-span-2">
           <Link rel="noreferrer noopener" href="/" className="font-bold text-xl flex">
             <Image
               src={theme === "system" ? LogoDark : theme === "dark" ? LogoDark : LogoLight}
