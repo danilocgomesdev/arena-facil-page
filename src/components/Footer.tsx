c9db0282e94a3727a8d4d7f5b009fefaf19ef068
Danilo Gomes <danilogomes@fieg.com.br>
2024-09-25 09:20:05 -0300

    add ajustes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
   return (
     <footer id="footer " className="flex flex-col items-center">
       <hr className="w-11/12 mx-auto" />
-      <section className="container py-20 pl-5  grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
+      <section className="container py-16 pl-5  grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
         <div className="col-span-full md:col-span-1 xl:col-span-2">
           <Link rel="noreferrer noopener" href="/" className="font-bold text-xl flex">
             <Image
@@ -26,7 +26,7 @@ export const Footer = () => {
 
           <div>
             <a rel="noreferrer noopener" href="#" className="opacity-60 hover:opacity-100">
-              Github
+              Facebook
             </a>
           </div>
 
